4aaca56070929ab653238c2d23da76d42dc8c627
Baylee Mumma <mumma103@mail.chapman.edu>
2017-10-31 17:20:23 +0000

    modules

---
+++ taylor_approx.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Name: Baylee Mumma, Christine Outlaw, Kristen Peet
+# Email: mumma103@mail.chapman.edu
+# Course: PHYS220/MATH220/CPSC220 Fall 2017
+# Assignment: CLASSWORK 7
+
+import array_calculus
+
+def taylor(x, f, i, n):
+    
---
+++ test_taylor_approx.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# Name: Baylee Mumma, Christine Outlaw, Kristen Peet
+# Email: mumma103@mail.chapman.edu
+# Course: PHYS220/MATH220/CPSC220 Fall 2017
+# Assignment: CLASSWORK 7
+
