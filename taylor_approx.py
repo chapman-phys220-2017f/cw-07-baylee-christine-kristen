0483bb4f7e8ab100c13c55ea90e71b50d327fd11
Baylee Mumma <mumma103@mail.chapman.edu>
2017-11-07 09:03:28 +0000

    new graph

--- taylor_approx.py
+++ taylor_approx.py
@@ -4,7 +4,30 @@
 # Course: PHYS220/MATH220/CPSC220 Fall 2017
 # Assignment: CLASSWORK 7
 
-import array_calculus
+import array_calculus as ac
+import numpy as np
 
-def taylor(x, f, i, n):
-    
+def taylor(x,f,i,n):
+    y = len(x)
+    mat = ac.derivative(x[0],x[y-1],y)
+    fx = np.zeros(y) #array on zeros of length y
+    def tay_matrix():
+        matrix = np.eye(y)
+        while True:
+            matrix = np.dot(matrix,mat)
+            yield matrix
+    yint = np.arange(y)
+    m = tay_matrix()
+    fx[yint] = f[i]
+    array = x[i]
+    for k in range(1,n):
+        matrix = next(m)
+        fx += (np.dot(matrix,f)[i]*((x-array)**k)/(np.math.factorial(k)))
+    return (x,fx)
+
+
+
+
+
+
+
